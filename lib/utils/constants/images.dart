b4911477a5ec5f1c5a7502ebfadc4a61cf79a509
Piyal <syedanamul67@gmail.com>
2025-09-17 12:16:14 +0600

    Auth Flow - Authentication UI Kit

--- lib/utils/constants/images.dart
+++ lib/utils/constants/images.dart
@@ -1,25 +1,3 @@
 class AppImages {
-  static const doubleArrow = "assets/images/double_arrow.png";
-  static const balon = "assets/images/balon.svg";
-  static const balon2 = "assets/images/balon_2.svg";
-  static const balonBig = "assets/images/blob.svg";
-  static const microphone = "assets/images/microphone.png";
-  static const quote = "assets/images/quote.png";
-  static const double_quotes = "assets/images/double-quotes.png";
-  static const person = "assets/images/person.jpg";
-  static const playButton = "assets/images/play-button.png";
-  static const colorMic = "assets/images/color_mic.png";
-  static const logo = "assets/images/logo.png";
-  static const americaFlag = "assets/images/america_flag.png";
-  static const food = "assets/images/food.jpg";
-  static const culture = "assets/images/culture.jpg";
-  static const cultures = "assets/images/cultures.jpg";
-  static const birdJson = "assets/images/bird.json";
-  static const birdJson2 = "assets/images/bird2.json";
-  static const foxJson = "assets/images/fox.json";
-  static const foxJson1 = "assets/images/fox1.json";
-  static const foxJson2 = "assets/images/fox2.json";
-  static const foxJson3 = "assets/images/fox3.json";
-  static const foxJson4 = "assets/images/fox4.json";
-  static const foxJson5 = "assets/images/fox5.json";
+  // static const doubleArrow = "assets/images/double_arrow.png";
 }
